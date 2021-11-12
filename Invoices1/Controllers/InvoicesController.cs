673fd8d927f6007f2810f052d3d5648917a0dafa
Ex1Kx <thenewestekigoh@gmail.com>
2021-11-12 10:08:54 -0500

    [Ex1] Added The Post Method

--- Invoices1/Controllers/InvoicesController.cs
+++ Invoices1/Controllers/InvoicesController.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Invoices1.Models;
 
 namespace Invoices1.Controllers
 {
@@ -25,6 +26,8 @@ namespace Invoices1.Controllers
             string query = @"
                         select Id,Namei,Last_Name,Document_Id from 
                         Clients
+                        select Id,Id_Client, Cod from Invoice
+                        select Id, Id_Invoice, Descriptioni, Valuei from InvoicesDetails
             ";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("InvoicesAppCon");
@@ -42,6 +45,40 @@ namespace Invoices1.Controllers
                 }
             }
             return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Clients clients)
+        {
+            string query = @"
+                        insert into Clients 
+                        (Id,Namei,Last_Name,Document_Id) 
+                        values
+                         (@Id,@Namei,@Last_Name,@Document_Id) ;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", clients.Id);
+                    myCommand.Parameters.AddWithValue("@Namei", clients.Namei);
+                    myCommand.Parameters.AddWithValue("@Last_Name", clients.Last_Name);
+                    myCommand.Parameters.AddWithValue("@Document_Id", clients.Document_Id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
         }
     }
 }
